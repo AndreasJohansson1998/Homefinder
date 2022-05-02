f330ee6cf9528e4372570d94ef0cf7c4a8df330a
AndreasJohansson1998 <98387120+AndreasJohansson1998@users.noreply.github.com>
2022-05-02 14:22:57 +0200

    First init

---
+++ House-API/Controllers/HouseController.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace House_API.Controllers
+{
+    [Route("[controller]")]
+    public class HouseController : Controller
+    {
+        private readonly ILogger<HouseController> _logger;
+
+        public HouseController(ILogger<HouseController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View("Error!");
+        }
+    }
+}
